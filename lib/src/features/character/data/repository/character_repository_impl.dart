4d1e8b5e1be88351aa8db2fac35f612406710306
Florentin <florentin.bouchez@gmail.com>
2025-03-14 23:19:48 +0100

    feat: implement infinite scroll

---
+++ lib/src/features/character/data/repository/character_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:infinite_scroll/src/features/character/character.dart';
+
+class CharacterRepositoryImpl extends CharacterRepository {
+  @override
+  Future<List<Character>> fetchAll({required int page}) async {
+      // Remote call.
+      final CharacterRemote remote = CharacterRemote();
+      final List<CharacterModel> characters = await remote.fetchAll(page);
+
+      // Result.
+      return characters.map((character) => character.toEntity()).toList();
+  }
+}
